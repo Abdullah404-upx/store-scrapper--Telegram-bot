d5efcd9e4ed0920fd6cdaf23a18fe3b22434dca8
abdullah404-upx <abudllah.ebrahim404@gmail.com>
2021-08-22 18:35:14 +0200

    ho

--- souq.js
+++ souq.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
 
 const souq = async (searchQuery) => {
   
-    const browser = await puppeteer.launch({headless: true});  
+    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox','--disable-setuid-sandbox']});  
   const page = await browser.newPage();
 
   await page.goto(`https://egypt.souq.com/eg-ar/${searchQuery}/s/?as=2&section=2&page=1`);
